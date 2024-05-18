9badf213cfec0a239f30a60e14106b9723a46466
Pedro Magalhaes <phmagalhaes013@gmail.com>
2024-05-18 12:16:29 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,7 +9,9 @@ public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(obj);
+        System.out.println(seller);
 
     }
 }
